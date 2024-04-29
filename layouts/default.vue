1796acdebec8be438c743994b67ab319a057c24b
Bruce Shih <sp1986@gmail.com>
2024-04-29 14:06:45 +0800

    feat: add layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <UContainer class="p-6">
+    <NuxtPage />
+  </UContainer>
+
+  <UNotifications />
+  <!-- <AppFooter /> -->
+</template>
--- pages/terms.client.vue
+++ pages/terms.client.vue
@@ -1,20 +1,18 @@
 <template>
-  <UContainer class="p-6">
-    <h1 class="text-2xl mb-8">
-      Terms
-    </h1>
-    <p class="mb-4">
-      This is a hobby project intended for personal/internal use. The source code can be found at my GitHub.
-    </p>
-    <p class="mb-4">
-      If you find it helpful feel free to contact me at
-      <ULink
-        to="https://bruceshih.me"
-        active-class="text-primary"
-        inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
-      >
-        here.
-      </ULink>
-    </p>
-  </UContainer>
+  <h1 class="text-2xl mb-8">
+    Terms
+  </h1>
+  <p class="mb-4">
+    This is a hobby project intended for personal/internal use. The source code and can be found at my GitHub.
+  </p>
+  <p class="mb-4">
+    If you find it helpful feel free to contact me at
+    <ULink
+      to="https://bruceshih.me"
+      active-class="text-primary"
+      inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+    >
+      here.
+    </ULink>
+  </p>
 </template>
